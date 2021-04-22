15a7f4a145d5ebd6113fc69772940161b0f7fcc9
rickblanc02 <rickblanc02@gmail.com>
2021-04-21 20:09:03 -0400

    Update TestWebApp.java

--- src/test/java/com/example/apptour/TestWebApp.java
+++ src/test/java/com/example/apptour/TestWebApp.java
@@ -1,26 +1,15 @@
 package com.example.apptour;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.example.apptour.models.Product;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class TestWebApp extends ApptourApplicationTests {
 
@@ -46,13 +35,6 @@ public class TestWebApp extends ApptourApplicationTests {
 		mockMvc.perform(get("/api/v1/products/p")).andExpect(status().isOk());
 				
 	}
-	
-	@Test
-	public void testProdutcsSave() throws Exception {
-		mockMvc.perform(post("/api/v1/products")).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json;charset=UTF-8"))
-				.andExpect(jsonPath("$.name").value("emp1"));
-
-	}
+		
 		
 }
